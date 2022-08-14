1157c387d35650691914341cd3194b24d7dfdf2e
Victoria Lojek <victoria.lojek@web.de>
2022-08-14 19:28:54 +0200

    Add valence

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const rwClient = require('./twitterClient.js');
 const CronJob = require('cron').CronJob;
 const axios = require('axios');
 const qs = require('qs');
-const skz = ['🐺', '🐰', '🐷🐰', '🥟', '🐥', '🐶', '🦊'];
-let randomSkz = skz[Math.floor(Math.random() * skz.length)];
+// const skz = ['🐺', '🐰', '🐷🐰', '🥟', '🐥', '🐶', '🦊'];
+// let randomSkz = skz[Math.floor(Math.random() * skz.length)];
+
+// Run cron job:
+const cronJob = new CronJob('0,15,30,45,55 * * * *', () => {
+  console.log('CronJob starting!');
+  tweet();
+});
 
 const client_id = '4e1009ef493441ce890eb3f17716f2cc';
 const client_secret = 'af53c639a0154bffb90e7488cb7f1fb9';
@@ -40,17 +46,19 @@ const getAuth = async () => {
 
 // Use Spotify Recommended:
 // Atributes Info: https://ahsieh53632.github.io/music-attributes-and-popularity/
-const getAudioFeatures_Track = async (energy) => {
+const getAudioFeatures_Track = async (energy, valence) => {
   const access_token = await getAuth();
   console.log('Access Token: ' + access_token);
 
   const api_url =
     `https://api.spotify.com/v1/recommendations?limit=3&market=DE&seed_artists=` +
-    `4D9foUQxTrsS0w2BeyCD16` +
+    `4D9foUQxTrsS0w2BeyCD16` + // SKZ
     `&seed_genres=k-pop&seed_tracks=` +
-    `279HOB7dgskHprPl4W4zAb` +
+    `279HOB7dgskHprPl4W4zAb` + // Placebo <3
     `&target_energy=` +
-    energy;
+    energy +
+    `&target_valence=` +
+    valence;
   try {
     const response = await axios.get(api_url, {
       headers: {
@@ -73,16 +81,32 @@ const tweet = async () => {
       'https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric&appid=b72392f28d8647a7b1bae67aff19abc1'
     );
     const temp = data.main.temp;
-    const weather = data.weather[0].description;
-    // Weather calc valence
-
-    //
+    let weather = data.weather[0].description;
+    // Calculate valence ('happiness') based on current weather. Weather Conditions: https://openweathermap.org/weather-conditions
+    let valenceOptions = [];
+    if ((weather === 'clear sky') | (weather === 'few clouds')) {
+      valenceOptions = [1, 0.95, 0.9, 0.85];
+    }
+    if ((weather === 'scattered clouds') | (weather === 'broken clouds')) {
+      valenceOptions = [0.8, 0.75, 0.7, 0.65];
+    }
+    if ((weather === 'shower rain') | (weather === 'rain')) {
+      valenceOptions = [0.6, 0.55, 0.5, 0.45];
+    }
+    if ((weather === 'thunderstorm') | (weather === 'rain')) {
+      valenceOptions = [0.4, 0.35, 0.3, 0.25];
+    }
+    if ((weather === 'snow') | (weather === 'mist')) {
+      valenceOptions = [0.2, 0.15, 0.1, 0.05, 0];
+    }
+    const valence =
+      valenceOptions[Math.floor(Math.random() * valenceOptions.length)];
+    console.log('Valence: ' + valenceOptions + ', Random: ' + valence);
+    // Calculate energy based on time of day:
     const time = new Date(data.dt * 1000).toLocaleTimeString('de-DE', {
       hour: '2-digit',
       minute: '2-digit',
     });
-
-    // Calculate energy based on time of day:
     const currentHours = new Date(data.dt * 1000).getHours();
     const sunsetHours = new Date(data.sys.sunset * 1000).getHours();
     const sunriseHours = new Date(data.sys.sunrise * 1000).getHours();
@@ -96,7 +120,7 @@ const tweet = async () => {
     console.log('Energy: ' + energy);
 
     // Run Spotify functions:
-    const recommended = await getAudioFeatures_Track(energy);
+    const recommended = await getAudioFeatures_Track(energy, valence);
     const recommendedArtist = recommended.tracks[0].artists[0].name;
     const recommendedTrack = recommended.tracks[0].name;
     const recommendedUrl = recommended.tracks[0].external_urls.spotify;
@@ -105,16 +129,20 @@ const tweet = async () => {
       recommendedTrack,
       recommendedUrl
     );
+
     // Define tweet content:
     await rwClient.v2.tweet(
-      randomSkz +
-        ' It is ' +
+      'It is ' +
         time +
-        '. Looks like the weather is ' +
+        ', and the weather is a dose of ' +
         weather +
         ' at ' +
         temp +
-        '°C. Based on that, I recommend you listen to: ' +
+        '°C. Based on that (Energy: ' +
+        energy +
+        ', Valence: ' +
+        valence +
+        '), I think you might like: ' +
         recommendedTrack +
         ' by ' +
         recommendedArtist +
@@ -127,11 +155,5 @@ const tweet = async () => {
     console.error(error);
   }
 };
-tweet();
-
-// Cron job:
-const job = new CronJob('0,15,30,45,55 * * * *', () => {
-  console.log('Cron job starting!');
-  tweet();
-});
-job.start();
+tweet(); // Manual tweet job.
+cronJob.start(); // Automated every 15 min tweet job.
