9ca0eee0e45f82bb90f423b311c9b7084d90b276
Victoria Lojek <victoria.lojek@web.de>
2022-08-15 00:07:00 +0200

    take hours from time api instead of weather api

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const rwClient = require('./twitterClient.js');
 const CronJob = require('cron').CronJob;
 const axios = require('axios');
 const qs = require('qs');
-// const skz = ['🐺', '🐰', '🐷🐰', '🥟', '🐥', '🐶', '🦊'];
-// let randomSkz = skz[Math.floor(Math.random() * skz.length)];
 
-// Run cron job:
-const cronJob = new CronJob('0,15,30,45,55 * * * *', () => {
+// Run Cron Job for scheduled runs:
+const cronJob = new CronJob('0,30 * * * *', () => {
   console.log('CronJob starting!');
   tweet();
 });
 
+// Build Spotify Auth Token
 const client_id = '4e1009ef493441ce890eb3f17716f2cc';
 const client_secret = 'af53c639a0154bffb90e7488cb7f1fb9';
 const auth_token = Buffer.from(
@@ -18,7 +17,7 @@ const auth_token = Buffer.from(
   'utf-8'
 ).toString('base64');
 
-// Get Spotify Access Token:
+// Refresh Spotify Access Token:
 const getAuth = async () => {
   const BASE64_ENCODED_AUTH_CODE = auth_token;
   const headers = {
@@ -49,7 +48,6 @@ const getAuth = async () => {
 const getAudioFeatures_Track = async (energy, valence) => {
   const access_token = await getAuth();
   console.log('Access Token: ' + access_token);
-
   const api_url =
     `https://api.spotify.com/v1/recommendations?limit=3&market=DE&seed_artists=` +
     `4D9foUQxTrsS0w2BeyCD16` + // SKZ
@@ -85,29 +83,35 @@ const tweet = async () => {
     // Calculate valence ('happiness') based on current weather. Weather Conditions: https://openweathermap.org/weather-conditions
     let valenceOptions = [];
     if ((weather === 'clear sky') | (weather === 'few clouds')) {
-      valenceOptions = [1, 0.95, 0.9, 0.85];
+      valenceOptions = [1, 0.975, 0.95, 0.925, 0.9, 0.875, 0.85, 0.825];
     }
     if ((weather === 'scattered clouds') | (weather === 'broken clouds')) {
-      valenceOptions = [0.8, 0.75, 0.7, 0.65];
+      valenceOptions = [0.8, 0.775, 0.75, 0.725, 0.7, 0.675, 0.65, 0.625];
     }
     if ((weather === 'shower rain') | (weather === 'rain')) {
-      valenceOptions = [0.6, 0.55, 0.5, 0.45];
+      valenceOptions = [0.6, 0.575, 0.55, 0.525, 0.5, 0.475, 0.45, 0.425];
     }
     if ((weather === 'thunderstorm') | (weather === 'rain')) {
-      valenceOptions = [0.4, 0.35, 0.3, 0.25];
+      valenceOptions = [0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25, 0.225];
     }
     if ((weather === 'snow') | (weather === 'mist')) {
-      valenceOptions = [0.2, 0.15, 0.1, 0.05, 0];
+      valenceOptions = [0.2, 0.175, 0.15, 0.125, 0.1, 0.075, 0.05, 0.025, 0];
     }
     const valence =
       valenceOptions[Math.floor(Math.random() * valenceOptions.length)];
     console.log('Valence: ' + valenceOptions + ', Random: ' + valence);
-    // Calculate energy based on time of day:
-    const time = new Date(data.dt * 1000).toLocaleTimeString('de-DE', {
-      hour: '2-digit',
-      minute: '2-digit',
-    });
-    const currentHours = new Date(data.dt * 1000).getHours();
+    // Calculate energy based on time of day - the closer to midday based on sunlight hours, the more energy:
+    const countryInfo = await axios(
+      'http://worldtimeapi.org/api/timezone/Europe/Berlin'
+    );
+    const time = new Date(countryInfo.data.unixtime * 1000).toLocaleTimeString(
+      'de-DE',
+      {
+        hour: '2-digit',
+        minute: '2-digit',
+      }
+    );
+    const currentHours = new Date(countryInfo.data.unixtime * 1000).getHours();
     const sunsetHours = new Date(data.sys.sunset * 1000).getHours();
     const sunriseHours = new Date(data.sys.sunrise * 1000).getHours();
     const zenithHours = new Date(
@@ -128,10 +132,11 @@ const tweet = async () => {
       `Artist Output: ${recommendedArtist}, ${recommendedTrack}, ${recommendedUrl}.`
     );
 
-    // Define tweet content:
+    // Draft final tweet:
     await rwClient.v2.tweet(
       `It's ${time}, and the weather is a dose of ${weather} at ${temp}°C. ` +
-        `Based on that (Energy: ${energy}, Valence: ${valence}, I think you might like: ` +
+        `Based on that (Energy: ${energy.toFixed(1)}, ` +
+        `Valence: ${valence.toFixed(1)}), I think you might like: ` +
         ` ${recommendedTrack} by ${recommendedArtist}. Here is a link! ${recommendedUrl}.`
     );
     console.log('Tweet Tweet');
@@ -139,5 +144,5 @@ const tweet = async () => {
     console.error(error);
   }
 };
-tweet(); // Manual tweet job.
-cronJob.start(); // Automated every 15 min tweet job.
+tweet(); // Manual tweet job from localhost & heroku start.
+cronJob.start(); // Automated every 30 min tweet job.
